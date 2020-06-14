40fc660173fd1b475dbcff4967de924151a2fd71
Dirk Jansen <dj.jansen@casema.nl>
2020-06-14 17:16:37 +0200

    Add files via upload

---
+++ Backup/pgdump-custom.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set PGPASSWORD=postgres45
+
+echo.
+echo.
+echo.
+echo Druk op een toets om een backup te maken van de database cashregister.
+pause > nul
+echo.
+"C:\ProgramData\Postgres\bin\pg_dump.exe" -U postgres -v --verbose -d  cashregister -Fc -f  "D:\Sales\Backup\cashregister.backup"
+echo.
+echo.
+echo Backup van de database cashregister is gemaakt.
+echo.
+echo Druk een toets om af te sluiten.
+pause > nul
+#-Fp Plain
+#-Fc Custom
+#-Fd directory
+#-Ft tar
