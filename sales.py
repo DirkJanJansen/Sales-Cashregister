2528319cbc8e20bddd371d3cfb8d38b9e83cefc7
Dirk Jansen <dj.jansen@casema.nl>
2023-03-17 12:26:39 +0100

    Add files via upload

--- sales.py
+++ sales.py
@@ -3127,9 +3127,6 @@ def calculationStock():
     engine = create_engine('postgresql+psycopg2://postgres@localhost/cashregister')
     con = engine.connect()
     myear = int(str(datetime.date.today())[0:4])
-    updeven = update(params).where(params.c.paramID == 4).values(value = int(myear%2))
-    con.execute(updeven)
-
     selpar = select([params]).order_by(params.c.paramID)
     rppar = con.execute(selpar).fetchall()
 
