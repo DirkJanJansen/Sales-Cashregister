456520a08db3462479a210b5148941662a7708c1
Dirk Jansen <dj.jansen@casema.nl>
2023-03-17 19:54:21 +0100

    Add files via upload

--- reset_odd_even.py
+++ reset_odd_even.py
@@ -4,12 +4,12 @@ from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine,\
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMessageBox, QApplication
 
-def odd_even():
+def odd_even(message):
     msg = QMessageBox()
     msg.setStyleSheet("font: 10pt Arial; color: black;  background-color: gainsboro")
     msg.setWindowIcon(QIcon('./images/logos/logo.jpg'))
     msg.setIcon(QMessageBox.Information)
-    msg.setText('Reset parameter successful!')
+    msg.setText(message)
     msg.setWindowTitle('Reset parameter')
     msg.exec_()
 
@@ -22,12 +22,16 @@ params = Table('params', metadata,
 engine = create_engine('postgresql+psycopg2://postgres@localhost/cashregister')
 con = engine.connect()
 
-myear = int(str(datetime.date.today())[0:4])
-updeven = update(params).where(params.c.paramID == 4).values(value = int(myear%2))
-con.execute(updeven)
+try:
+    myear = int(str(datetime.date.today())[0:4])
+    updeven = update(params).where(params.c.paramID == 4).values(value = int(myear%2))
+    con.execute(updeven)
+    message = "Reset parameter successful!"
+except:
+    message = "Reset parameter failed"
 
 app = QApplication(sys.argv)
 app.setStyle("Windows")
-sys.exit(odd_even())
+sys.exit(odd_even(message))
 app.exec_()
 
